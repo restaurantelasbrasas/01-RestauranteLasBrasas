98c1a64e64d3587ab34089bc9659e70727561ca3
Camila Sampson <camilagsampr@gmail.com>
2019-07-24 09:00:20 -0600

    Pantallas nuevas, falta usuarios y conectar todo al menu

---
+++ 01-RestauranteLasBrasas/WindowGestionarProducto.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _01_RestauranteLasBrasas
+{
+    /// <summary>
+    /// Lógica de interacción para WindowGestionarProducto.xaml
+    /// </summary>
+    public partial class WindowGestionarProducto : Window
+    {
+        public WindowGestionarProducto()
+        {
+            InitializeComponent();
+        }
+    }
+}
